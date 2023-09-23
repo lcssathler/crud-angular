2054936143f2be021dce39b6eca42599cbeb80da
Lucas Sathler <lcssathler@gmail.com>
2023-09-22 21:42:17 -0300

    pulling values from courses attribute

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    { _id: "1", name: "Expert Front-End Developer", category: "Front-End" },
+    { _id: "1", name: "Full Stack Java Developer", category: "Full Stack" },
+    { _id: "1", name: "How to have a healthier life", category: "Health" }
+  ];
+  displayedColumns = ["name", "category"];
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
