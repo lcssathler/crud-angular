75e95683883599a359b5fa36e5438c919aad6338
Lucas Sathler <lcssathler@gmail.com>
2023-11-08 20:47:24 -0300

    java bean validation on java entity

--- crud-spring/src/main/java/com/br/crudcourses/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/br/crudcourses/crudspring/controller/CourseController.java
@@ -2,12 +2,19 @@ package com.br.crudcourses.crudspring.controller;
 
 import com.br.crudcourses.crudspring.model.Course;
 import com.br.crudcourses.crudspring.repository.CourseRepository;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
@@ -23,7 +30,7 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id) {
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
         return courseRepository.findById(id)
                 .map(response -> ResponseEntity.ok().body(response))
                 .orElse(ResponseEntity.notFound().build());
@@ -31,29 +38,29 @@ public class CourseController {
 
     @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course createCourse(@RequestBody Course course) {
+    public Course createCourse(@RequestBody @Valid Course course) {
         return courseRepository.save(course);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
         return courseRepository.findById(id)
                 .map(courseFound -> {
                     courseFound.setName(course.getName());
                     courseFound.setCategory(course.getCategory());
                     Course updated = courseRepository.save(courseFound);
-                    return ResponseEntity.ok().body(courseFound);
+                    return ResponseEntity.ok().body(updated);
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
         return courseRepository.findById(id)
-            .map(courseFound -> {
-                courseRepository.deleteById(courseFound.getId());
-                return ResponseEntity.noContent().<Void>build();
-            })
-            .orElse(ResponseEntity.notFound().build());
+                .map(courseFound -> {
+                    courseRepository.deleteById(courseFound.getId());
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- crud-spring/src/main/java/com/br/crudcourses/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/br/crudcourses/crudspring/model/Course.java
@@ -1,7 +1,12 @@
 package com.br.crudcourses.crudspring.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +21,17 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("_id")
     private Long id;
-    @Column(length = 200, nullable = false)
+
+    @Column(length = 100, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
     private String name;
-    @Column(length = 200, nullable = false)
+
+    @Column(length = 10, nullable = false)
+    @NotBlank
+    @NotNull
+    @Pattern(regexp = "Back-End|Front-End")
     private String category;
 
     public Course(String name, String category) {
