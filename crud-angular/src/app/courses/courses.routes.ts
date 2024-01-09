1157dc180523fb17cf2ba515d2c4c154af9b4c66
Lucas Sathler <lcssathler@gmail.com>
2024-01-08 22:34:37 -0300

    standalone migration - remove modules

--- crud-angular/src/app/courses/courses.routes.ts
+++ crud-angular/src/app/courses/courses.routes.ts
@@ -1,17 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CoursesComponent } from './containers/courses/courses.component';
 import { CourseFormComponent } from './containers/course-form/course-form.component';
+import { CoursesComponent } from './containers/courses/courses.component';
+import { Routes } from "@angular/router";
 import { courseResolver } from './guards/course.resolver';
 
-const routes: Routes = [
+export const COURSES_ROUTES: Routes = [
   { path: "", component: CoursesComponent },
   { path: "new", component: CourseFormComponent, resolve: { course: courseResolver } },
   { path: "edit/:id", component: CourseFormComponent, resolve: { course: courseResolver } }
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class CoursesRoutingModule { }
